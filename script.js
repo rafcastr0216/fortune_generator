800db5a73068a1b6cca13c2c8db97ef89887138d
Rafael Castro <rgcs1602@gmail.com>
2021-01-13 20:38:43 -0600

    changed some of the responses

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //console.log('Hello User here is your fortune');
 const structures = {
     starters: ['Hello ', 'Hi ', 'Nice to see you '],
-    middle: ['to answer your question: ', 'I think that ', 'Chances are that '],
+    middle: ['Well ', 'I think that ', 'Chances are that '],
     ending: ['Luck is on your side', 'I do not have an answer for that', 'It will happen', 'Sorry not likely']
 };
 const fortune = (user, question) => {
@@ -9,8 +9,8 @@ const fortune = (user, question) => {
     let j = Math.floor(Math.random()*3);
     let n = Math.floor(Math.random()*4);
 
-    let sentence = structures.starters[i] + user + ' ' + 'You ask ' + question + ' ' + structures.middle[j] + structures.ending[n];
+    let sentence = structures.starters[i] + user + ', ' + 'You asked: ' + question + ' your answer is: ' + structures.middle[j] + structures.ending[n];
     return sentence;
 };
 
-console.log(fortune(Raul, will I find love?))
+console.log(fortune('Raul', 'will I find love?'))
